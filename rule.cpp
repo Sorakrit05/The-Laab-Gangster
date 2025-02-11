012f3c3fba0628a8265922225a6f182ed3e10dfd
Sorakrit05 <aomsin22122558@gmail.com>
2025-02-11 15:59:08 +0700

    UD2

---
+++ rule.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "1. When starting the game, the person who ate the last Laab in the group will start first.\n\n";
+
+    cout << "2. When it's their turn, the players roll all 4 dice to choose and pick the ingredients according to the color they rolled.\n\n";
+
+    cout << "3. Dice roll results\n\n";
+
+    cout << "-If the result shows 2 or more of the same color, the player must pick 1 event card. Near the picking of the ingredient coin, the event card will show the result immediately after picking.\n\n";
+
+    cout << "4. If the player has received all the ingredients of that color as desired, the player can choose to pick ingredients from other colors instead.\n\n";
+
+    cout << "5. End of the game The player who receives all the ingredients according to the Laab recipe first will be the winner. And you are the\n\n";
+
+    cout << "REAL_Laab_gangster\n\n";
+
+}
