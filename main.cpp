0351d9b6361296247f42cfc193c569e9337eb236
Anasuki0310 <anatsapong0310@gmail.com>
2025-02-14 22:13:39 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,149 +1,182 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <ctime>
 #include <cstdlib>
+#include <ctime>
+#include <map>
+#include <set>
+#include <algorithm>
 
 using namespace std;
 
-vector<string> meats = {"เนื้อวัว", "เนื้อหมู", "เนื้อไก่"};
-vector<string> herbs = {"ใบสะระแหน่", "ผักชี", "ต้นหอม"};
-vector<string> spices = {"พริกป่น", "ข้าวคั่ว", "น้ำปลา"};
-vector<string> vegetables = {"ผักกาดขาว", "แตงกวา", "หอมแดง"};
-
-
-vector<string> destinyCards = {
-    "ได้ส่วนผสมเพิ่ม 1 ชิ้น", "เสียส่วนผสม 1 ชิ้น", "ขโมยส่วนผสมจากผู้เล่นอื่น",
-    "แลกส่วนผสมกับผู้เล่นอื่น", "ได้ทอยลูกเต๋าอีกครั้ง", "เสียตาถัดไป"
+struct Ingredient {
+    string name;
+    string type;
+    int quantity = 4;
 };
 
+vector<string> colors = {"Red", "Orange", "Brown", "Green"};
+map<string, string> ingredientMap = {
+    {"Red", "Meat"},
+    {"Orange", "Spices"},
+    {"Brown", "Herbs"},
+    {"Green", "Vegetables"}
+};
 
-string drawDestinyCard() {
-    int index = rand() % destinyCards.size();
-    return destinyCards[index];
-}
+vector<Ingredient> meats = {
+    {"Beef", "meat", 4},
+    {"Pork", "meat", 4},
+    {"Beef broth", "meat", 4},
+    {"Fried garlic", "meat", 4},
+    {"Buffalo", "meat", 4},
+    {"Bile", "meat", 4},
+    {"Blood", "meat", 4},
+    {"Offal", "meat", 4}
+};
 
+vector<Ingredient> herbs = {
+    {"Makwaen", "herb", 4},
+    {"Coriander", "herb", 4},
+    {"Nut meg", "herb", 4},
+    {"Star Anise", "herb", 4},
+    {"black pepper", "herb", 4},
+    {"Long pepper", "herb", 4},
+    {"Cinnamon", "herb", 4},
+    {"Clove", "herb", 4}
+};
 
-vector<string> rollDice(int numDice) {
-    vector<string> colors = {"แดง", "น้ำตาล", "ส้ม", "เขียว"};
-    vector<string> result;
-    for (int i = 0; i < numDice; ++i) {
-        int index = rand() % colors.size();
-        result.push_back(colors[index]);
-    }
-    return result;
-}
+vector<Ingredient> vegetables = {
+    {"Matoom", "vegetable", 4},
+    {"KaowTong", "vegetable", 4},
+    {"Oreille", "vegetable", 4},
+    {"Deeplygung", "vegetable", 4},
+    {"Makwen-infused fish sauce", "vegetable", 4},
+    {"Dried Fennel Seed", "vegetable", 4},
+    {"Phak paem", "vegetable", 4},
+    {"Vietnamese Coriander", "vegetable", 4},
+    {"Damocles tree", "vegetable", 4},
+    {"Parsley", "vegetable", 4},
+    {"Para cress", "vegetable", 4},
+    {"Olive", "vegetable", 4}
+};
 
+vector<Ingredient> spice = {
+    {"Makwaen", "spice", 4},
+    {"Fragrant", "spice", 4},
+    {"Fennel Seed", "spice", 4},
+    {"Dried Galangal", "spice", 4},
+    {"Cardamom", "spice", 4},
+    {"Chili Spur Pepper", "spice", 4},
+    {"Caraway", "spice", 4}
+};
 
-bool hasDuplicates(vector<string> diceResult) {
-    sort(diceResult.begin(), diceResult.end());
-    auto last = unique(diceResult.begin(), diceResult.end());
-    return last != diceResult.end();
-}
-
+vector<string> fortuneCards = {"Suan Krua", "Moitgure", "Aoa Mue", "Hom Bun", "Regular Customer", "Moldy Cutting Board", "Hom poy", "Ngan Katin", "The Kitchen", "Rainy Season", "Broken Mortar", "Flipped Tray"};
+map<string, int> ingredients = {
+    {"Meat", 8 * 4},
+    {"Herbs", 8 * 4},
+    {"Spices", 7 * 4},
+    {"Vegetables", 12 * 4}
+};
 
 struct Player {
     string name;
-    string lastAteLab;
+    int lastLarbMonth;
     vector<string> recipe;
-    vector<string> ingredients;
+    map<string, int> collectedIngredients;
 };
 
+vector<Player> players;
+int currentPlayer = 0;
+
+void drawFortuneCard() {
+    int randomIndex = rand() % fortuneCards.size();
+    string selectedCard = fortuneCards[randomIndex];
+    
+    cout << "Gangster's Fortune Card: " << selectedCard << endl;
+}
+
+void rollDice(Player &player) {
+    vector<string> diceResults;
+    set<string> uniqueColors;
+    
+    cout << player.name << " is rolling 4 dice...\n";
+    for (int i = 0; i < 4; i++) {
+        int roll = rand() % colors.size();
+        diceResults.push_back(colors[roll]);
+        uniqueColors.insert(colors[roll]);
+    }
+    
+    cout << "Dice results: ";
+    for (const string& color : diceResults) {
+        cout << color << " ";
+    }
+    cout << endl;
+    
+    if (uniqueColors.size() == 4) {
+        cout << "You got 4 different colors! Pick one ingredient of each color.\n";
+        for (const string& color : uniqueColors) {
+            if (ingredients[ingredientMap[color]] > 0) {
+                player.collectedIngredients[ingredientMap[color]]++;
+                ingredients[ingredientMap[color]]--;
+            }
+        }
+    } else {
+        cout << "You rolled duplicate colors! Drawing a fortune card...\n";
+        drawFortuneCard();
+    }
+}
+
+void setupGame() {
+    int numPlayers;
+    do {
+        cout << "Enter number of players (max 4): ";
+        cin >> numPlayers;
+        if (numPlayers > 4) cout << "Too many players! Please enter again.\n";
+    } while (numPlayers > 4);
+    
+    players.resize(numPlayers);
+    
+    for (int i = 0; i < numPlayers; i++) {
+        cout << "Enter name of player " << i + 1 << ": ";
+        cin >> players[i].name;
+        cout << "Enter months since " << players[i].name << " last ate larb: ";
+        cin >> players[i].lastLarbMonth;
+    }
+    
+    sort(players.begin(), players.end(), [](const Player &a, const Player &b) {
+        return a.lastLarbMonth < b.lastLarbMonth;
+    });
+    
+    cout << "Turn order: \n";
+    for (const auto& player : players) {
+        cout << player.name << "\n";
+    }
+}
+
+void gameLoop() {
+    while (true) {
+        Player &player = players[currentPlayer];
+        rollDice(player);
+        
+        bool hasWon = true;
+        for (const auto &ingredient : player.recipe) {
+            if (player.collectedIngredients[ingredient] < 1) {
+                hasWon = false;
+                break;
+            }
+        }
+        
+        if (hasWon) {
+            cout << player.name << " has collected all ingredients and won the game!\n";
+            break;
+        }
+        
+        currentPlayer = (currentPlayer + 1) % players.size();
+    }
+}
 
 int main() {
     srand(time(0));
-
-    cout << "1. When starting the game, the person who ate the last Laab in the group will start first.\n";
-
-    cout << "2. When it's their turn, the players roll all 4 dice to choose and pick the ingredients according to the color they rolled.\n";
-
-    cout << "3. Dice roll results\n";
-
-    cout << "-If the result shows 2 or more of the same color, the player must pick 1 event card. Near the picking of the ingredient coin, the event card will show the result immediately after picking.\n\n";
-
-    cout << "4. If the player has received all the ingredients of that color as desired, the player can choose to pick ingredients from other colors instead.\n\n";
-
-    cout << "5. End of the game The player who receives all the ingredients according to the Laab recipe first will be the winner. And you are the\n\n";
-
-    cout << "REAL_gangster\n";
-
-    int numPlayers;
-    cout << "เลือกจำนวนผู้เล่น (ไม่เกิน 4 คน): ";
-
-    cin >> numPlayers;
-
-
-    vector<Player> players(numPlayers);
-    for (int i = 0; i < numPlayers; ++i) {
-        cout << "ผู้เล่น " << i + 1 << " กรุณาใส่ชื่อของคุณ: ";
-        cin >> players[i].name;
-        cout << "คุณกินลาบครั้งล่าสุดเมื่อไหร่: ";
-        cin >> players[i].lastAteLab;
-    }
-
-   
-    int currentPlayerIndex = rand() % numPlayers;
-    cout << "ผู้เล่นที่เริ่มต้นคือ: " << players[currentPlayerIndex].name << endl;
-
- 
-    bool gameOver = false;
-    while (!gameOver) {
-        Player &currentPlayer = players[currentPlayerIndex];
-        cout << "ตาของ " << currentPlayer.name << endl;
-
-        vector<string> diceResult = rollDice(4);
-        cout << "ผลลัพธ์ลูกเต๋า: ";
-        for (string color : diceResult) {
-            cout << color << " ";
-        }
-        cout << endl;
-
-        if (hasDuplicates(diceResult)) {
-            cout << "ได้สีซ้ำกัน ต้องเปิดการ์ดดวง!" << endl;
-            string card = drawDestinyCard();
-            cout << "การ์ดดวง: " << card << endl;
-     
-        } else {
-            cout << "เลือกส่วนผสมตามสีที่ได้:" << endl;
-            for (string color : diceResult) {
-                if (color == "แดง") {
-                    cout << "เลือกเนื้อ: ";
-                    for (int i = 0; i < meats.size(); ++i) {
-                        cout << i + 1 << ". " << meats[i] << " ";
-                    }
-                    int choice;
-                    cin >> choice;
-                    currentPlayer.ingredients.push_back(meats[choice - 1]);
-                } else if (color == "น้ำตาล") {
-                    cout << "เลือกสมุนไพร: ";
-                    for (int i = 0; i < herbs.size(); ++i) {
-                        cout << i + 1 << ". " << herbs[i] << " ";
-                    }
-                    int choice;
-                    cin >> choice;
-                    currentPlayer.ingredients.push_back(herbs[choice - 1]);
-                } else if (color == "ส้ม") {
-                    cout << "เลือกเครื่องเทศ: ";
-                    for (int i = 0; i < spices.size(); ++i) {
-                        cout << i + 1 << ". " << spices[i] << " ";
-                    }
-                    int choice;
-                    cin >> choice;
-                    currentPlayer.ingredients.push_back(spices[choice - 1]);
-                } else if (color == "เขียว") {
-                    cout << "เลือกผัก: ";
-                    for (int i = 0; i < vegetables.size(); ++i) {
-                        cout << i + 1 << ". " << vegetables[i] << " ";
-                    }
-                    int choice;
-                    cin >> choice;
-                    currentPlayer.ingredients.push_back(vegetables[choice - 1]);
-                }
-            }
-        }
-        currentPlayerIndex = (currentPlayerIndex + 1) % numPlayers;
-    }
-
+    setupGame();
+    gameLoop();
     return 0;
 }
